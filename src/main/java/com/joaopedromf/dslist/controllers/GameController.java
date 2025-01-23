52ce98f9cad172fe7a4cc5c11c6707a0553661cb
João Pedro Martins Forte <jpforte2004@gmail.com>
2025-01-23 18:07:56 -0300

    Domain model

--- src/main/java/com/joaopedromf/dslist/controllers/GameController.java
+++ src/main/java/com/joaopedromf/dslist/controllers/GameController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joaopedromf.dslist.dto.GameMinDTO;
-import com.joaopedromf.dslist.entities.Game;
 import com.joaopedromf.dslist.services.GameService;
 
 @RestController
