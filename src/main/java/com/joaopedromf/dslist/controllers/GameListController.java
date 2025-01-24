8b0032ab8a16c9e7f2a5715ddcedca7f3571f1b4
João Pedro Martins Forte <jpforte2004@gmail.com>
2025-01-23 21:42:43 -0300

    Move games on list

--- src/main/java/com/joaopedromf/dslist/controllers/GameListController.java
+++ src/main/java/com/joaopedromf/dslist/controllers/GameListController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joaopedromf.dslist.dto.GameListDTO;
 import com.joaopedromf.dslist.dto.GameMinDTO;
+import com.joaopedromf.dslist.dto.ReplacementDTO;
 import com.joaopedromf.dslist.services.GameListService;
 import com.joaopedromf.dslist.services.GameService;
 
@@ -33,5 +36,10 @@ public class GameListController {
     public List<GameMinDTO> findByList(@PathVariable long listId){
         List<GameMinDTO> result = gameService.findByList(listId);
         return result;
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable long listId, @RequestBody ReplacementDTO body){
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 }
